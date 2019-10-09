01adf5349a988dceab04247d21de3616045ef623
xfeng3 <xfeng3@nd.edu>
2019-10-09 15:32:15 -0400

    hjsdf

--- exercise2.sh
+++
@@ -1,6 +0,0 @@
-highest=$(sort -n  -k4 -t , wages.csv | cut -d , -f1,2,4 | tail -n 1)
-echo "the gender, yearsExperience, and wage for the highest earner:$highest"
-lowest=$(sort -n -k4 -t , wages.csv | cut -d , -f1,2,4 | head -n 2 | tail -n 1)
-echo "the gender, yearsExperience, and wage for the lowest earner:$lowest"
-femaleN=$(sort -n -k4 -t , wages.csv | cut -d , -f1,2,4 | tail -n 10 | grep -w female | wc -l)
-echo "the number of females in the top ten earners:$femaleN"
